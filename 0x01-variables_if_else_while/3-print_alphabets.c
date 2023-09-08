95c0f2fb493767fb4d9f112eda979c1442c1ece3
OtmaneTachalte <otmanetachalte@gmail.com>
2023-09-08 05:37:27 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,13 @@ int main(void)
 {
 	char ch;
 
+	// Print lowercase alphabet
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
-	
+
+	// Print uppercase alphabet
 	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
 		putchar(ch);
