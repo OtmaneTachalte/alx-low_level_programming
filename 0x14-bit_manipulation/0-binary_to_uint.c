c48e49c8a9f34e87277cdc3e0902b36137d4aa6e
OtmaneTachalte <otmanetachalte@gmail.com>
2023-10-26 12:44:27 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,20 +9,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int num = 0;
+	unsigned int num = 0;
 
-    if (!b)
-        return (0);
+	if (!b)
+		return (0);
 
-    while (*b)
-    {
-        num <<= 1;
-        if (*b == '1')
-            num += 1;
-        else if (*b != '0')
-            return (0);
-        b++;
-    }
+	while (*b)
+	{
+		num <<= 1;
+		if (*b == '1')
+			num += 1;
+		else if (*b != '0')
+			return (0);
+		b++;
+	}
 
-    return (num);
+	return (num);
 }
