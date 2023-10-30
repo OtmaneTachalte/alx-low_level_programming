24b3b2382f0b0256250445cec88269dd3ee021e1
OtmaneTachalte <otmanetachalte@gmail.com>
2023-10-30 13:52:18 -0700

    3cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,103 +1,127 @@
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #define BUFFER_SIZE 1024
 
-void check_arguments(int argc);
-int open_source_file(char *file_name);
-int open_destination_file(char *file_name);
-void copy_content(int file_from, int file_to);
-void close_files(int file_from, int file_to);
+/* Function Prototypes */
+void handle_read_error(char *file);
+void handle_write_error(char *file);
+void handle_close_error(int fd);
+void copy_file_contents(int src_fd, int dest_fd,
+char *src_file, char *dest_file);
+int open_source_file(char *file);
+int create_dest_file(char *file);
 
 /**
- * main - Copies the content of a file to another file.
+ * main - Entry point, copies contents of one file to another.
  * @argc: Argument count.
  * @argv: Argument vector.
- * Return: Always 0.
+ * Return: 0 on success, exit codes for various errors.
  */
 int main(int argc, char *argv[])
 {
 	int file_from, file_to;
 
-	check_arguments(argc);
-	file_from = open_source_file(argv[1]);
-	file_to = open_destination_file(argv[2]);
-	copy_content(file_from, file_to);
-	close_files(file_from, file_to);
-
-	return (0);
-}
-
-void check_arguments(int argc)
-{
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+
+	file_from = open_source_file(argv[1]);
+	file_to = create_dest_file(argv[2]);
+
+	copy_file_contents(file_from, file_to, argv[1], argv[2]);
+
+	if (close(file_from) == -1)
+		handle_close_error(file_from);
+	if (close(file_to) == -1)
+		handle_close_error(file_to);
+	return (0);
 }
 
-int open_source_file(char *file_name)
+/**
+ * handle_read_error - Handles read errors, prints and exits.
+ * @file: The filename that couldn't be read.
+ */
+void handle_read_error(char *file)
 {
-	int file_from = open(file_name, O_RDONLY);
-
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_name);
-		exit(98);
-	}
-	return (file_from);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+	exit(98);
 }
 
-int open_destination_file(char *file_name)
+/**
+ * handle_write_error - Handles write errors, prints and exits.
+ * @file: The filename that couldn't be written to.
+ */
+void handle_write_error(char *file)
 {
-	int file_to = open(file_name, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
-		exit(99);
-	}
-	return (file_to);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+	exit(99);
 }
 
-void copy_content(int file_from, int file_to)
+/**
+ * handle_close_error - Handles close errors, prints and exits.
+ * @fd: The file descriptor that couldn't be closed.
+ */
+void handle_close_error(int fd)
 {
-	int r, w;
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+}
+
+/**
+ * copy_file_contents - Copies content from one file to another.
+ * @src_fd: File descriptor of source file.
+ * @dest_fd: File descriptor of destination file.
+ * @src_file: Source file name for error handling.
+ * @dest_file: Destination file name for error handling.
+ */
+void copy_file_contents(int src_fd, int dest_fd,
+						char *src_file, char *dest_file)
+{
+	ssize_t read_bytes, written_bytes;
 	char buffer[BUFFER_SIZE];
 
-	while ((r = read(file_from, buffer, BUFFER_SIZE)) > 0)
+	while ((read_bytes = read(src_fd, buffer, BUFFER_SIZE)) > 0)
 	{
-		w = write(file_to, buffer, r);
-		if (w != r)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-			close(file_from);
-			close(file_to);
-			exit(99);
-		}
+		written_bytes = write(dest_fd, buffer, read_bytes);
+		if (written_bytes != read_bytes)
+			handle_write_error(dest_file);
 	}
 
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+	if (read_bytes == -1)
+		handle_read_error(src_file);
 }
 
-void close_files(int file_from, int file_to)
+/**
+ * open_source_file - Opens a file for reading.
+ * @file: The filename to open.
+ * Return: File descriptor of opened file.
+ */
+int open_source_file(char *file)
 {
-	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
+	int fd;
 
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		handle_read_error(file);
+	return (fd);
+}
+
+/**
+ * create_dest_file - Creates a file for writing.
+ * @file: The filename to create.
+ * Return: File descriptor of created file.
+ */
+int create_dest_file(char *file)
+{
+	int fd;
+
+	fd = open(file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd == -1)
+		handle_write_error(file);
+	return (fd);
 }
